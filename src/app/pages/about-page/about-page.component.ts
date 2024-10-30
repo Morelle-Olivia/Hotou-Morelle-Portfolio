221eb4d3a78d84ca06f78f489c2816ed9bfacb4b
Morelle-Olivia <hotoumorelle@gmail.com>
2024-10-30 14:55:20 +0100

    Added translation

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterModule} from '@angular/router';
+import {TranslateModule} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [RouterModule, RouterLink],
+  imports: [RouterModule, RouterLink, TranslateModule],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss'
 })
